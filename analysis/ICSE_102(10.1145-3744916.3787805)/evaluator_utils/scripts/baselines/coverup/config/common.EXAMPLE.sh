6684fed1bc293872cab555754719c2af9070521f
j-saha <sj.sahajoy@gmail.com>
2026-06-07 16:18:18 -0400

    Remove ICSE_102 analysis folder

--- analysis/ICSE_102(10.1145-3744916.3787805)/evaluator_utils/scripts/baselines/coverup/config/common.EXAMPLE.sh
+++
@@ -1,6 +0,0 @@
-#
-# If you create a file named "common.sh" in this directory, it will be executed by
-# scripts/run_coverup.sh for all configurations
-#
-# You can use it to set your OpenAI or other keys:
-#export OPENAI_API_KEY="sk-..."
